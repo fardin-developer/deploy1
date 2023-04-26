16059e53636f245d446733ba822552e91a1200ef
fardin-developer <105560328+fardin-developer@users.noreply.github.com>
2023-04-26 10:31:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
- const express = require('express')
+const express = require('express');
+const path = require('path');
 
-    const app = express()
 
-    app.get('/', (req, res) => {
-        res.send('Express JS on Vercel')
-    })
+const app = express()
 
-    app.get('/ping', (req, res) => {
-        res.send('pong 🏓')
-    })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
 
-    const port = process.env.PORT || 8080
 
-    app.listen(port, (err, res) => {
-        if (err) {
-            console.log(err)
-            return res.status(500).send(err.message)
-        } else {
-            console.log('[INFO] Server Running on port:', port)
-        }
-    })
+
+app.get('/ping', (req, res) => {
+    res.send('pong 🏓')
+})
+
+const port = process.env.PORT || 8080
+
+app.listen(port, (err, res) => {
+    if (err) {
+        console.log(err)
+        return res.status(500).send(err.message)
+    } else {
+        console.log('[INFO] Server Running on port:', port)
+    }
+})
